14e77feec50341e532fa4340348932a4930823b0
BenedictAbania <abaniachristian@gmail.com>
2025-11-13 14:49:40 +0800

    update

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ function About() {
       <h2>About Me</h2>
       <p>
         I’m a passionate and detail-oriented professional with a strong focus on creativity, collaboration, and continuous learning. 
-        I enjoy turning ideas into impactful results—whether through design, development, or problem-solving. 
+        I enjoy turning ideas into impactful results, whether through design, development, or problem-solving. 
         With a balance of technical skills and a keen eye for aesthetics, 
         I aim to create meaningful and user-centered work that makes a difference.
       </p>
