c6fc24f0de2d21cc815aac487cdf2e87040989bc
BenedictAbania <abaniachristian@gmail.com>
2025-11-13 15:23:40 +0800

    Updates

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,11 @@ import React from 'react';
 function Home() {
   return (
     <header className="section" id="home">
+      <img 
+        src="/images/profile-pic.jpg" 
+        alt="A description of you" 
+        className="profile-picture"
+      />
       <h1>BENEDICT ABANIA</h1>
       <p>Frontend Developer | React Enthusiast | Lifelong Learner</p>
     </header>
